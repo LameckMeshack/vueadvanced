c8a3f20524821bd5927e52c43143620de81f8030
RhinolamerSonwar <rhinolamer@gmail.com>
2022-08-31 18:58:27 +0300

    defined the asynComp

---
+++ src/components/AsyncComponents/AsyncComp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <AsyncComponent />
+  </div>
+</template>
+<script>
+import Loading from "./LoadingComponent.vue";
+import HelloWord from "./HelloComponent.vue";
+import Error from "./ErrorComponent";
+const AsyncComponent = () => ({
+  component: new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(HelloWord);
+    }, 1000);
+  }),
+  loading: Loading,
+  error: Error,
+  delay: 0,
+  timeout: 3000,
+});
+export default {
+  name: "App",
+  components: {
+    AsyncComponent,
+  },
+};
+</script>
+<style></style>
